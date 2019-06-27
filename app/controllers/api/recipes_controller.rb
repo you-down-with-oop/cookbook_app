30c492491343365937da1f7a21c389a7c6e19747
Peter Jang <peter.x.jang@gmail.com>
2019-06-27 18:25:17 -0500

    Add RESTful recipes update action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -19,4 +19,16 @@ class Api::RecipesController < ApplicationController
     @recipe = Recipe.find_by(id: params["id"])
     render "show.json.jb"
   end
+
+  def update
+    @recipe = Recipe.find_by(id: params["id"])
+    @recipe.title = params["title"] || @recipe.title
+    @recipe.chef = params["chef"] || @recipe.chef
+    @recipe.ingredients = params["ingredients"] || @recipe.ingredients
+    @recipe.directions = params["directions"] || @recipe.directions
+    @recipe.image_url = params["image_url"] || @recipe.image_url
+    @recipe.prep_time = params["prep_time"] || @recipe.prep_time
+    @recipe.save
+    render "show.json.jb"
+  end
 end
