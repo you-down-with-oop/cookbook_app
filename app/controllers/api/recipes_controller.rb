798450264a2144441aacab4148f516cac455ee44
Peter Jang <peter.x.jang@gmail.com>
2019-06-26 21:24:09 -0500

    Add RESTful recipes create action

--- app/controllers/api/recipes_controller.rb
+++ app/controllers/api/recipes_controller.rb
@@ -4,6 +4,17 @@ class Api::RecipesController < ApplicationController
     render "index.json.jb"
   end
 
+  def create
+    @recipe = Recipe.new(
+      title: params["title"],
+      chef: params["chef"],
+      ingredients: params["ingredients"],
+      directions: params["directions"],
+    )
+    @recipe.save
+    render "show.json.jb"
+  end
+
   def show
     @recipe = Recipe.find_by(id: params["id"])
     render "show.json.jb"
