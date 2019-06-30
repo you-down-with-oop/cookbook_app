a6e9abc5a23f0e1073e8dcdda2bd7b3deec4bd54
Peter Jang <peter.x.jang@gmail.com>
2019-06-30 09:31:44 -0500

    Add model methods to format attributes for the frontend

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,22 @@
 class Recipe < ApplicationRecord
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(", ")
+  end
+
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hour(s) " if hours > 0
+    result += "#{minutes} minute(s)" if minutes > 0
+    result
+  end
 end
