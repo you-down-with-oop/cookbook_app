54477565e42a10d62a2c56a7682e26daac5e38f3
Peter Jang <peter.x.jang@gmail.com>
2019-07-07 10:43:26 -0500

    Add login action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     post "/users" => "users#create"
+    post "/sessions" => "sessions#create"
 
     get "/recipes" => "recipes#index"
     post "/recipes" => "recipes#create"
