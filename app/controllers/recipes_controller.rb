5bc97a89466119b8b32c175ea027803fc8f83417
Peter Jang <peter.x.jang@gmail.com>
2019-07-15 20:50:36 -0500

    Add recipes create HTML action with links

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,6 +8,20 @@ class RecipesController < ApplicationController
     render "new.html.erb"
   end
 
+  def create
+    @recipe = Recipe.new(
+      user_id: 1,
+      title: params[:input_title],
+      chef: params[:input_chef],
+      prep_time: params[:input_prep_time],
+      ingredients: params[:input_ingredients],
+      directions: params[:input_directions],
+      image_url: params[:input_image_url],
+    )
+    @recipe.save
+    redirect_to "/recipes"
+  end
+
   def show
     @recipe = Recipe.find_by(id: params[:id])
     render "show.html.erb"
